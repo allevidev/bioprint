bbd8049577f25151c576c65611850ea9b95d6424
Karan Hiremath <karanhiremath@Karans-MacBook-Pro-2.local>
2017-03-07 10:01:21 -0800

    updating windows install

--- Windows/Install.bat
+++ Windows/Install.bat
@@ -9,7 +9,8 @@ cd ../bioprint
 C:\Python27\Scripts\pip.exe install virtualenv
 call virtualenv -p C:\Python27\python.exe biobots
 cd ../bioprint/biobots/Scripts & activate 
-cd ../../ & pip install -r bioprint-requirements.txt
+cd ../../
+call pip install -r bioprint-requirements.txt
 call easy_install bioprint-bioprint-1.5-py2.7.egg
 
 pause
