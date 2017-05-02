7605d6abd8965006a2ad8cecd557b26a835a824f
Karan Hiremath <karan.hiremath@gmail.com>
2017-05-02 14:02:12 -0700

    Update Install.bat

--- Windows/Install.bat
+++ Windows/Install.bat
@@ -4,13 +4,8 @@ echo 127.0.0.1 bioprint >> %hostspath%
 
 setx path "%path%;C:\Python27;C:\Python27\Scripts"
 
+pushd %~dp0
 cd ../bioprint
-
 C:\Python27\Scripts\pip.exe install virtualenv
-call virtualenv -p C:\Python27\python.exe biobots
-cd ../bioprint/biobots/Scripts & activate 
-cd ../../
-call pip install -r bioprint-requirements.txt
-call easy_install bioprint-bioprint-1.5-py2.7.egg
-
-pause
+C:\Python27\Scripts\virtualenv.exe -p C:\Python27\python.exe biobots
+cd biobots/Scripts & activate  & cd ../../ & pip install -r bioprint-requirements.txt & easy_install bioprint-bioprint-1.5-py2.7.egg & pause
